d74bc35a9f20a0f5b354dcb4f5bf20057f9a6cd7
SHAVITGH1 <shavitp21@gmail.com>
2026-01-17 14:03:49 +0200

    Removed Dist folder and fixed compilition

--- dist/controllers/healthController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHealth = void 0;
-const getHealth = (req, res) => {
-    res.json({ status: 'ok' });
-};
-exports.getHealth = getHealth;
--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import * as bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 import User, { IUser } from '../models/User';
 import Session from '../models/Session';
